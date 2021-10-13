4b6206f7b7ccf8189ac247de2cbf94bf7dfd9870
controlol <laluckyluc7@gmail.com>
2021-10-13 14:40:28 +0200

    set does not work, add catch environment

--- 10-prepare-rclone.sh
+++ 10-prepare-rclone.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/execlineb -P
 with-contenv
 
-set -eu
-
 if [ ! -f "/config/rclone.conf" ]; then
+  echo "Creating rclone config file"
+
+  # check if environment variables are set
+  if [ -z "${PASSWORD}" ]; then
+    echo "Please set PASSWORD environment"
+    exit 1
+  fi
+  if [ -z "${PASSWORD2}" ]; then
+    echo "Please set PASSWORD2 environment"
+    exit 1
+  fi
+  if [ -z "${RCLONE_FOLDER}" ]; then
+    echo "Please set RCLONE_FOLDER environment"
+    exit 1
+  fi
+  if [ -z "${RCLONE_REMOTE}" ]; then
+    echo "Please set RCLONE_REMOTE environment"
+    exit 1
+  fi
+
+  # chekc if base config file exists
   if [ ! -f "/config/gdrive-rclone.conf" ]; then
     echo "Missing gdrive-rclone.conf file!"
     exit 1
@@ -23,6 +42,8 @@ if [ ! -f "/config/rclone.conf" ]; then
 
     rclone config create "rclone_remote" crypt remote=$remote password=$pwobscure password2=$pwobscurehash
   # done
+
+  echo "Creating rclone service"
 
   # create gdrive-rclone service
   cd /gdrive-services
