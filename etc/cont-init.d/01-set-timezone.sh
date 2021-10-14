53a27964bcaad303d2efff50df331ad62c32c86b
controlol <46456214+controlol@users.noreply.github.com>
2021-10-14 09:24:00 +0200

    Update 01-set-timezone.sh

--- etc/cont-init.d/01-set-timezone.sh
+++ etc/cont-init.d/01-set-timezone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 
-timezone="${TZ:-"Europe/Amsterdam"}"
+timezone="${TZ:-Europe/Amsterdam}"
 
 ln -snf /usr/share/zoneinfo/$timezone /etc/localtime
 echo $timezone > /etc/timezone
