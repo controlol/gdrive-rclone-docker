ddde206579ac150d0ba10179e645547e6d862e36
controlol <46456214+controlol@users.noreply.github.com>
2021-10-26 10:44:49 +0200

    support for no-encrypt
    allows setting the NO_CRYPT env.
    If this variable is not empty files will not be encrypted
    
    Also there were some syntax changes in if statements
    
    add colon to rclone_remote variable and remove the colon from the crontab to support folders without writing to seperate commands
    
    Do not run 'sed' on finish file as there is nothing to replace

--- etc/cont-init.d/10-prepare-rclone.sh
+++ etc/cont-init.d/10-prepare-rclone.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/with-contenv bash
 set -eu
 
-rclone_remote="$RCLONE_REMOTE-crypt-$RCLONE_FOLDER"
-
+# copy gdrive-rclone.conf and check if the $RCLONE_REMOTE config exists
 if [ ! -f "/config/rclone.conf" ]; then
   echo "Creating rclone config file"
 
-  # chekc if base config file exists
+  # check if base config file exists
   if [ ! -f "/config/gdrive-rclone.conf" ]; then
     echo "Missing gdrive-rclone.conf file!"
     exit 1
@@ -15,28 +14,32 @@ if [ ! -f "/config/rclone.conf" ]; then
   # copy the base config
   cp /config/gdrive-rclone.conf /config/rclone.conf
 
-  echo "Creating remote config $rclone_remote"
+  if ! grep -q "[$RCLONE_REMOTE]" /config/rclone.conf; then
+    echo "Config $RCLONE_REMOTE does not exist in gdrive-rclone.conf"
+    exit 1
+  fi
+fi
 
-  # create obscure passwords
-  pwobscure=$(/usr/bin/rclone obscure "$PASSWORD")
-  pwobscurehash=$(/usr/bin/rclone obscure "$PASSWORD2")
+if [ -z "$NO_CRYPT" ]; then
+  # add the encrypted remote
+  rclone_remote="$RCLONE_REMOTE-crypt-$RCLONE_FOLDER"
 
-  remote="$RCLONE_REMOTE:/$RCLONE_FOLDER"
+  if ! grep -q "[$rclone_remote]" /config/rclone.conf; then
+    # remote does not exist but rclone.conf does exist, create new remote
+    echo "Adding remote config $rclone_remote"
 
-  # create the config
-  /usr/bin/rclone config create "$rclone_remote" crypt remote="$remote" password="$pwobscure" password2="$pwobscurehash"
-elif [[ -z $(cat /config/rclone.conf | grep "$rclone_remote") ]]; then
-  # remote does not exist but rclone.conf does exist, create new remote
-  echo "Creating remote config $rclone_remote"
+    # create obscure passwords
+    pwobscure=$(/usr/bin/rclone obscure "$PASSWORD")
+    pwobscurehash=$(/usr/bin/rclone obscure "$PASSWORD2")
 
-  # create obscure passwords
-  pwobscure=$(/usr/bin/rclone obscure "$PASSWORD")
-  pwobscurehash=$(/usr/bin/rclone obscure "$PASSWORD2")
+    remote="$RCLONE_REMOTE:/$RCLONE_FOLDER"
 
-  remote="$RCLONE_REMOTE:/$RCLONE_FOLDER"
-
-  # create the config
-  /usr/bin/rclone config create "$rclone_remote" crypt remote="$remote" password="$pwobscure" password2="$pwobscurehash"
+    # create the config
+    /usr/bin/rclone config create "$rclone_remote" crypt remote="$remote" password="$pwobscure" password2="$pwobscurehash"
+  fi
+else
+  # use unencrypted folder inside the base folder
+  rclone_remote="$RCLONE_REMOTE:/$RCLONE_FOLDER"
 fi
 
 if [ ! -f /setupcontainer ]; then
@@ -44,7 +47,13 @@ if [ ! -f /setupcontainer ]; then
   unset PASSWORD
   unset PASSWORD2
 
-  # create folders 
+  # add colon to reclone_remote if type is crypt
+  # if this is not done files are copied to local disk and not to the remote
+  if [ -z "$NO_CRYPT" ]; then
+    rclone_remote="$rclone_remote:"
+  fi
+
+  # create folders
   # the merged fs - local cache for gdrive - new local only files
   mkdir -p /local/{cache,gdrive} /config/log
 
@@ -55,13 +64,15 @@ if [ ! -f /setupcontainer ]; then
   echo "Creating rclone service file"
   cd /etc/services.d/rclone || exit 1
 
-  sed -i "s,<cache-size>,$LOCAL_CACHE_SIZE,g" run finish
-  sed -i "s,<cache-time>,$LOCAL_CACHE_TIME,g" run finish
-  sed -i "s,<gdrive-rclone>,$rclone_remote,g" run finish
+  sed -i "s,<cache-size>,$LOCAL_CACHE_SIZE,g" run
+  sed -i "s,<cache-time>,$LOCAL_CACHE_TIME,g" run
+  sed -i "s,<gdrive-rclone>,$rclone_remote,g" run
 
   echo "Creating cron task"
   mkdir -p /etc/crontabs
-  echo "0 */6 * * * /usr/bin/rclone move /local/gdrive $rclone_remote: --config /config/rclone.conf --log-file /var/log/rclone/upload.log --log-level INFO --delete-empty-src-dirs --drive-stop-on-upload-limit --fast-list --min-age 6h" > /etc/crontabs/root
+
+  echo "0 */6 * * * /usr/bin/rclone move /local/gdrive $rclone_remote --config /config/rclone.conf --log-file /var/log/rclone/upload.log --log-level INFO --delete-empty-src-dirs --drive-stop-on-upload-limit --fast-list --min-age 6h" > /etc/crontabs/root
+
   crontab /etc/crontabs/root
 
   # so we know the container has already been setup
